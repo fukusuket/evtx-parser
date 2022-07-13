0b85281f9817b68d791a0ace51d676f801b15610
fukusuket <fukusuke.takahashi@gmail.com>
2022-07-14 06:57:02 +0900

    fix unused variable.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use std::fs::File;
-use std::io::BufWriter;
-use std::path::PathBuf;
 use clap::Parser;
 use csv::{QuoteStyle, WriterBuilder};
 use evtx::EvtxParser;
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-
     #[clap(short, long, value_parser, value_name = "Directory")]
     dir: Option<PathBuf>,
 
@@ -16,19 +15,27 @@ struct Args {
     file: Option<PathBuf>,
 }
 
-
-fn main()  -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     let _args = Args::parse();
 
     // Change this to a path of your .evtx sample.
-    let fp = PathBuf::from(format!("{}/samples/system.evtx", std::env::var("CARGO_MANIFEST_DIR").unwrap()));
+    let fp = PathBuf::from(format!(
+        "{}/samples/system.evtx",
+        std::env::var("CARGO_MANIFEST_DIR").unwrap()
+    ));
 
     let mut parser = EvtxParser::from_path(fp).unwrap();
     let o = File::create("out.csv")?;
-    let mut wtr = WriterBuilder::new().quote_style(QuoteStyle::Always).from_writer(BufWriter::new(o));
-    for (i, record) in parser.records_json_value().enumerate() {
+    let mut wtr = WriterBuilder::new()
+        .quote_style(QuoteStyle::Always)
+        .from_writer(BufWriter::new(o));
+    for (_, record) in parser.records_json_value().enumerate() {
         let r = record.unwrap();
-        let _ = wtr.write_record([r.event_record_id.to_string(), r.timestamp.to_string(), r.data.to_string()]);
+        let _ = wtr.write_record([
+            r.event_record_id.to_string(),
+            r.timestamp.to_string(),
+            r.data.to_string(),
+        ]);
     }
     Ok(())
 }
