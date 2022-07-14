ed6a4df05ffc00d3f0f86695a826058532dcc2da
fukusuket <fukusuke.takahashi@gmail.com>
2022-07-15 00:07:32 +0900

    parse all evtx in directory.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use csv::{QuoteStyle, WriterBuilder};
 use evtx::EvtxParser;
+use std::fs;
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::PathBuf;
@@ -8,34 +9,44 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    #[clap(short, long, value_parser, value_name = "Directory")]
+    #[clap(short, long, value_parser)]
     dir: Option<PathBuf>,
 
-    #[clap(short, long, value_parser, value_name = "File")]
+    #[clap(short, long, value_parser)]
     file: Option<PathBuf>,
 }
 
 fn main() -> std::io::Result<()> {
     let _args = Args::parse();
 
-    // Change this to a path of your .evtx sample.
     let fp = PathBuf::from(format!(
-        "{}/samples/system.evtx",
+        "{}/samples/",
         std::env::var("CARGO_MANIFEST_DIR").unwrap()
     ));
 
-    let mut parser = EvtxParser::from_path(fp).unwrap();
-    let o = File::create("out.csv")?;
-    let mut wtr = WriterBuilder::new()
-        .quote_style(QuoteStyle::Always)
-        .from_writer(BufWriter::new(o));
-    for (_, record) in parser.records_json_value().enumerate() {
-        let r = record.unwrap();
-        let _ = wtr.write_record([
-            r.event_record_id.to_string(),
-            r.timestamp.to_string(),
-            r.data.to_string(),
-        ]);
+    for entry in fs::read_dir(fp)? {
+        let entry = entry?;
+        let path = entry.path();
+        let filename = String::from(entry.file_name().to_string_lossy());
+        let mut parser = EvtxParser::from_path(path).unwrap();
+        let mut wtr = WriterBuilder::new()
+            .quote_style(QuoteStyle::Always)
+            .from_writer(BufWriter::new(File::create(format!(
+                "./target/{}.csv",
+                filename
+            ))?));
+        for record in parser.records_json_value() {
+            match record {
+                Ok(r) => {
+                    let _ = wtr.write_record([
+                        r.event_record_id.to_string(),
+                        r.timestamp.to_string(),
+                        r.data.to_string(),
+                    ]);
+                }
+                Err(e) => eprintln!("{}", e),
+            }
+        }
     }
     Ok(())
 }
